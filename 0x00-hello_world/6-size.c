cc6fbabef17cd88c810658682522b6764e67a848
Adu Gyamfi Kwabena <adugyamfik33@gmail.com>
2022-10-13 07:56:06 -0700

    Hello World Sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@ char charType;
 float fltType;
 long long int lliType;
 long int liType;
-printf("Size of a char: %zu byte(s)\n",sizeof(charType));
-printf("Size of an int: %zu byte(s)\n",sizeof(intType));
-printf("Size of a long int: %zu byte(s)\n",sizeof(liType));
-printf("Size of a long long int: %zu byte(s)\n",sizeof(lliType));
-printf("Size of a float: %zu byte(s)\n",sizeof(fltType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(liType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(lliType));
+printf("Size of a float: %zu byte(s)\n", sizeof(fltType));
 return (0);
 }
