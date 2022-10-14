1eae385acc90439f549897e4b612b0052f3b57f1
Adu Gyamfi Kwabena <adugyamfik33@gmail.com>
2022-10-14 09:53:24 -0700

    Enhanced

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+
+int main(void)
+
+{
+
+        for(int i=0;i<8;i++)
+
+        {
+
+            for(int j=0;j<9;j++)
+
+            {
+
+                for(int k=0;k<10;k++)
+
+                {
+
+                    if(i == j || j == k || i == k)
+
+                        continue;
+
+                    putchar('0'+i);
+
+                    putchar('0'+j);
+
+                    putchar('0'+k);
+
+                    if(i == 7 && j == 8 && k == 9)
+
+                        continue;
+
+                    putchar(',');
+
+                    putchar(' ');
+
+                }
+
+            }
+
+        }
+
+	return (0);
+
+}
