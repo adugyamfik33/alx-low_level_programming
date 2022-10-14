3c4b9dc21d2c2859adab73a4fe8101fadbf64210
Adu Gyamfi Kwabena <adugyamfik33@gmail.com>
2022-10-14 09:18:36 -0700

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+int main(void)
+
+{
+
+    int x=102;
+
+        for(int i=0;i<10;i++)
+
+        {
+
+            putchar('0'+i);
+
+        }
+
+        for(int j =97;j<=102;j++)
+
+        {
+
+            putchar(j);
+
+        }
+
+        putchar('\n');
+
+	return (0);
+
+}
