e5d8d28fd6666988a3d00eab24a5893c4e175ae8
Adu Gyamfi Kwabena <adugyamfik33@gmail.com>
2022-10-14 04:59:31 -0700

    Alphabets-2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+int main(void)
+
+{
+
+    int z =122,y=90,x;
+
+        for(x = 97;x<=z;x++)
+
+        {
+
+            putchar(x);
+
+        }
+
+        for(x = 65;x<=y;x++)
+
+        {
+
+            putchar(x);
+
+        }
+
+        putchar('\n');
+
+	return (0);
+
+}
+
