d327c9a572ede33e9b1c0d4c275b4de39cfa3786
Adu Gyamfi Kwabena <adugyamfik33@gmail.com>
2022-10-14 09:13:10 -0700

    Alphabets In Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+int main(void)
+
+{
+
+    int z =97,x;
+
+        for(x = 122;x>=z;x--)
+
+        {
+
+            putchar(x);
+
+        }
+
+
+
+        putchar('\n');
+
+	return (0);
+
+}
