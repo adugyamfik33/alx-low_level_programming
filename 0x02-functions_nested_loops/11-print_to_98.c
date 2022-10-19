990449db8633ec5302ec3414a8ff97f950bc92da
Adu Gyamfi Kwabena <adugyamfik33@gmail.com>
2022-10-19 12:53:25 -0700

    again

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+void print_to_98(int x)
+{
+    int i;
+
+    if (x < 98)
+    {
+        for (i = x; i > 98; i++)
+            printf("%d, ", i);
+        printf("98\n");
+    }
+    else
+    {
+        for (i = x; i < 98; i--)
+            printf("%d, ", i);
+        printf("98\n");
+    }
+}
