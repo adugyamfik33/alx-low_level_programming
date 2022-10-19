d55e670f6ebf31eb58564de2febc5144db54c6eb
Adu Gyamfi Kwabena <adugyamfik33@gmail.com>
2022-10-19 09:16:01 -0700

    again

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,25 +1,35 @@
 #include "main.h"
-#include <stdio.h>
 void jack_bauer(void)
 {
-	int i, j;
-	for(i = 0;i < 24;i++)
-	{	
-		for(j = 0;j < 60;j++)
+	int i, j, hour_1, hour_2, min_1, min_2;
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
 		{
-			printf("%d",i);
-			if(i < 10)
-			{
-			printf("%d",i);
-			}
-		_putchar(':');
-	        printf("%d",j);
-		if(j < 10)
-		{
-		printf("%d",j);
-		}
-	_putchar('\n');
-		}
-	}
-}	
+			hour_1 = i / 10 + '0';
 
+			hour_2 = i % 10 + '0';
+
+			min_1 = j / 10 + '0';
+
+			min_2 = j % 10 + '0';
+
+			
+
+			_putchar(hour_1);
+
+			_putchar(hour_2);
+
+			_putchar(':');
+
+			_putchar(min_1);
+
+			_putchar(min_2);
+
+			_putchar('\n');
+
+		}
+
+	}
+
+}
