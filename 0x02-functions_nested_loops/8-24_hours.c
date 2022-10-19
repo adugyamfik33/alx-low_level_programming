ed001c175d39d773675bd972a79050dfff237341
Adu Gyamfi Kwabena <adugyamfik33@gmail.com>
2022-10-19 07:25:03 -0700

    hello

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+void jack_bauer(void)
+{
+	int i, j;
+	for(i = 0;i < 24;i++)
+	{	
+		for(j = 0;j < 60;j++)
+		{
+			_putchar(i+'0');
+			if(i < 10)
+			{
+			_putchar(i+'0');
+			}
+		_putchar(':');
+	        _putchar(j+'0');
+		if(j < 10)
+		{
+		_putchar(j+'0');
+		}
+	_putchar('\n');
+		}
+	}
+}	
+
