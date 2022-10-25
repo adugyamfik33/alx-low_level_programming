a3485267712af49e8b1d803e0f6be01002d22aba
Adu Gyamfi Kwabena <adugyamfik33@gmail.com>
2022-10-25 09:15:20 -0700

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_rev(char *s)
+{
+	int x;
+	x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	x--;
+	while (x >= 0)
+	{
+		_putchar(s[x]);
+		x--;
+	}
+	_putchar('\n');
+}
