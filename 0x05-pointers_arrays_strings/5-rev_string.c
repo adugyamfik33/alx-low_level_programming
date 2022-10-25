65d2fa88798633afde8b13fb4df97641257a3265
Adu Gyamfi Kwabena <adugyamfik33@gmail.com>
2022-10-25 09:28:36 -0700

    reversed string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,17 +7,17 @@ void rev_string(char *s)
 
 	x =0;
 	y =0;
-	char s_temp[1024];
+	char stemp[1024];
 
 	while (*(s + x))
 	{
-		*(s_temp + x) = *(s + x);
+		*(stemp + x) = *(s + x);
 		x++;
 	}
     x--;
 	while (x >= 0)
 	{
-		*(s + x) = *(s_temp + y);
+		*(s + x) = *(stemp + y);
 		x --;
 		y++;
 	}
